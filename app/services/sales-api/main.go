80aedc0cb4bbbc9454cae8f67e33f97cfca1f792
zwewintti <zwewintt24@gmail.com>
2023-01-07 11:36:42 +0630

    adding ardanlabs/config package

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
+	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -33,12 +38,67 @@ func main() {
 }
 
 func run(log *zap.SugaredLogger) error {
+	// ==========================================================================
+	// GOMAXPROCS
+
+	// Set the correct numbers of threads for the service
+	// based on what is available either by the machine or quotas
 	if _, err := maxprocs.Set(); err != nil {
 		return fmt.Errorf("max procs: %w", err)
 	}
 
 	log.Infow("start up", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// ==========================================================================
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "information",
+		},
+	}
+
+	//prefixing before config variables
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// ==========================================================================
+	// App Starting
+
+	log.Infow("starting service", "version", build)
+	defer log.Infow("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Infow("startup", "config", out)
+
+	// ==========================================================================
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
 	return nil
+
 }
 
 func initLogger(service string) (*zap.SugaredLogger, error) {
