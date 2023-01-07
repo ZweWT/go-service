b4f89f894cab29c287071a81effe1c6df773dcc2
zwewintti <zwewintt24@gmail.com>
2023-01-07 23:12:38 +0630

    shutting down the service gracefully

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"expvar"
 	"fmt"
@@ -114,10 +115,55 @@ func run(log *zap.SugaredLogger) error {
 		}
 	}()
 
-	// ==========================================================================
+	// =========================================================================
+	// Start API Service
+
+	log.Infow("startup", "status", "initializing API support")
+
+	// Make a channel to listen for an interrupted or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
+
+	// Construct a server to serve requests against the mux
+	api := http.Server{
+		Addr:         cfg.Web.APIHost,
+		Handler:      nil,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     zap.NewStdLog(log.Desugar()),
+	}
+
+	// Make a channel to listen for erros coming from the listeners. Use a
+	// bufferd channel so the go routine can exit if we don't collect the this error.
+	serverErrors := make(chan error, 1)
+
+	// Start the service listening for api requests
+	go func() {
+		log.Infow("startup", "status", "api router started", "host", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// =========================================================================
+	// Shutdown
+
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+
+	case sig := <-shutdown:
+		log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		if err := api.Shutdown(ctx); err != nil {
+			api.Close()
+			return fmt.Errorf("could not stop server gracefully: %w", err)
+		}
+	}
 
 	return nil
 
