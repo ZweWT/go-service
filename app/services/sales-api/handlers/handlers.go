69472a575d7557b7e13a08b783cf370d9c166081
zwewintti <zwewintt24@gmail.com>
2023-01-16 15:33:17 +0630

    readiness and liveness debug endpoint and test endpoint

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -6,8 +6,18 @@ import (
 	"expvar"
 	"net/http"
 	"net/http/pprof"
+	"os"
+
+	"github.com/ZweWT/go-service/app/services/sales-api/handlers/debug/checkgrp"
+	"github.com/ZweWT/go-service/app/services/sales-api/handlers/v1/testgrp"
+	"github.com/dimfeld/httptreemux/v5"
+	"go.uber.org/zap"
 )
 
+// DebugStandardLibraryMux registers all the debug routes from the standard library
+// into a new mux bypassing the use of the DefaultServerMux. Using the
+// DefaultServerMux would be a security risk since a dependency could inject a
+// handler into the service without us knowing it.
 func DebugStandardLibraryMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
@@ -17,6 +27,40 @@ func DebugStandardLibraryMux() *http.ServeMux {
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	mux.Handle("/debug/vars", expvar.Handler())
+
+	return mux
+}
+
+// DebugMux registers all the debug standard library routes and other custom
+// debug application routes for the service.
+func DebugMux(build string, log *zap.SugaredLogger) http.Handler {
+	mux := DebugStandardLibraryMux()
+
+	// Register debug check endpoints.
+	cgh := checkgrp.Handlers{
+		Build: build,
+		Log:   log,
+	}
+
+	mux.HandleFunc("/debug/readiness", cgh.Readiness)
+	mux.HandleFunc("/debug/liveness", cgh.Liveness)
+
+	return mux
+}
+
+type APIMuxConfig struct {
+	Shutdown chan os.Signal
+	Log      *zap.SugaredLogger
+}
+
+func APIMux(cfg APIMuxConfig) *httptreemux.ContextMux {
+	mux := httptreemux.NewContextMux()
+
+	tgh := testgrp.Handlers{
+		Log: cfg.Log,
+	}
+
+	mux.Handle(http.MethodGet, "/v1/test", tgh.Test)
 
 	return mux
 }
--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -105,7 +105,7 @@ func run(log *zap.SugaredLogger) error {
 	// related endpoints. This includes the standard library endpoints.
 
 	// Construct the mux for debug calls.
-	debugMux := handlers.DebugStandardLibraryMux()
+	debugMux := handlers.DebugMux(build, log)
 
 	//start the service listening for debug requests.
 
@@ -125,10 +125,14 @@ func run(log *zap.SugaredLogger) error {
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 
+	apiMux := handlers.APIMux(handlers.APIMuxConfig{
+		Shutdown: shutdown,
+		Log:      log,
+	})
 	// Construct a server to serve requests against the mux
 	api := http.Server{
 		Addr:         cfg.Web.APIHost,
-		Handler:      nil,
+		Handler:      apiMux,
 		ReadTimeout:  cfg.Web.ReadTimeout,
 		WriteTimeout: cfg.Web.WriteTimeout,
 		IdleTimeout:  cfg.Web.IdleTimeout,
